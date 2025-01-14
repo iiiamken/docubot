b79de89b1058bda17e381f8c52a433aeb76cea95
iiiamken <kenny.t.dinh93@gmail.com>
2025-01-14 23:48:59 +0100

    created step 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,6 +94,27 @@ export default function Home() {
             </p>
           </div>
         </div>
+        {/* steps */}
+        <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-blue-600">Step 1</span>
+              <span className="text-xl font-semibold">
+                Sign up for an account
+              </span>
+              <span className="mt-2 text-zinc-700">
+                Either starting out with a free plan or choose our{" "}
+                <Link
+                  href="/pricing"
+                  className="text-blue-700 underline underline-offset-2"
+                >
+                  pro plan
+                </Link>
+                .
+              </span>
+            </div>
+          </li>
+        </ol>
       </div>
     </>
   )
