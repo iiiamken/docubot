20b11b1b830e565e69ea9f424ee82174bd749562
iiiamken <kenny.t.dinh93@gmail.com>
2025-01-15 22:07:16 +0100

    created check for user and if not create new user in endpoint

--- src/app/server/index.ts
+++ src/app/server/index.ts
@@ -1,6 +1,7 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { publicProcedure, router } from "./trpc"
 import { TRPCError } from "@trpc/server"
+import { db } from "@/db"
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
     //check if user exist
@@ -9,7 +10,21 @@ export const appRouter = router({
     if (!user.id || !user.email) {
       throw new TRPCError({ code: "UNAUTHORIZED" })
     }
+    // check if user is in db
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    })
 
+    if (!dbUser) {
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      })
+    }
     return { success: true }
   }),
 })
