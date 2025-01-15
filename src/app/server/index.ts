3e6d09b01095e92282168887b0d10dd987f91d94
iiiamken <kenny.t.dinh93@gmail.com>
2025-01-15 21:15:36 +0100

    fixed bug in trpc server/index file

--- src/app/server/index.ts
+++ src/app/server/index.ts
@@ -2,10 +2,10 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { publicProcedure, router } from "./trpc"
 import { TRPCError } from "@trpc/server"
 export const appRouter = router({
-  authCallback: publicProcedure.query(() => {
+  authCallback: publicProcedure.query(async () => {
     //check if user exist
     const { getUser } = getKindeServerSession()
-    const user = getUser()
+    const user = await getUser()
     if (!user.id || !user.email) {
       throw new TRPCError({ code: "UNAUTHORIZED" })
     }
