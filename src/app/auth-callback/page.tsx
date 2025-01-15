47156954447a0b7ae9fb95c279c4a347dc51cd78
iiiamken <kenny.t.dinh93@gmail.com>
2025-01-15 20:16:21 +0100

    created route for authCallback

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,10 +1,18 @@
-import { useRouter } from "next/navigation"
+import { useRouter, useSearchParams } from "next/navigation"
+import { trpc } from "../_trpc/client"
 
-const page = () => {
+const Page = () => {
   const router = useRouter()
   const searchParams = useSearchParams()
 
   const origin = searchParams.get("origin")
+
+  const { isLoading, data } = trpc.authCallback.useQuery(undefined, {
+    onSuccess: ({ success }) => {
+      if (!success) return
+      router.push(origin ? `/${origin}` : "/dashboard")
+    },
+  })
 }
 
-export default page
+export default Page
