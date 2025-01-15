a40812d942dc3703aa92979b9b8c682adbfc72e7
iiiamken <kenny.t.dinh93@gmail.com>
2025-01-15 15:24:19 +0100

    added kinde session

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,11 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+
 const page = () => {
   return <h1>Dashboard</h1>
+
+  const { getUser } = getKindeServerSession()
+  const user = getUser()
+  console.log(user)
 }
 
 export default page
