1a87ad00171fa44ac1a68c68cdcf27f5d39e4658
iiiamken <kenny.t.dinh93@gmail.com>
2025-01-17 20:29:29 +0100

    added onUploadComplete function to add to db

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,3 +1,4 @@
+import { db } from "@/db"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { createUploadthing, type FileRouter } from "uploadthing/next"
 
@@ -5,7 +6,7 @@ const f = createUploadthing()
 
 export const ourFileRouter = {
   pdfUploader: f({
-    image: {
+    pdf: {
       maxFileSize: "4MB",
       maxFileCount: 1,
     },
@@ -20,7 +21,15 @@ export const ourFileRouter = {
       return { userId: user.id }
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      return {}
+      const createdFile = await db.file.create({
+        data: {
+          key: file.key,
+          name: file.name,
+          userId: metadata.userId,
+          url: file.url,
+          uploadStatus: "PROCESSING",
+        },
+      })
     }),
 } satisfies FileRouter
 
