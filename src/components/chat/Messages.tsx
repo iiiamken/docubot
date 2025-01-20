4fc6ce9e73ee2fc8a9f3dd5e3895d4e2b4d1c378
iiiamken <kenny.t.dinh93@gmail.com>
2025-01-20 23:37:47 +0100

    fetched messages using infinitequery in messages

--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -1,4 +1,20 @@
-const Messages = () => {
+import { trpc } from "@/app/_trpc/client"
+import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query"
+
+interface MessagesProps {
+  fileId: string
+}
+
+const Messages = ({ fileId }: MessagesProps) => {
+  trpc.getFileMessages.useInfiniteQuery(
+    {
+      fileId,
+      limit: INFINITE_QUERY_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage?.nextCursor,
+    }
+  )
   return <div></div>
 }
 
