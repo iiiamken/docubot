8de408c6331c071ca4618d1b8634621813163abd
iiiamken <kenny.t.dinh93@gmail.com>
2025-01-21 00:14:03 +0100

    added elements to isLoading elements

--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -1,6 +1,7 @@
 import { trpc } from "@/app/_trpc/client"
 import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query"
-import { Loader2 } from "lucide-react"
+import { Loader2, MessageSquare } from "lucide-react"
+import Skeleton from "react-loading-skeleton"
 
 interface MessagesProps {
   fileId: string
@@ -38,6 +39,10 @@ const Messages = ({ fileId }: MessagesProps) => {
     <div className="flex max-h-[calc(100vh-3.5rem-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
       {combinedMessages && combinedMessages.length > 0 ? (
         combinedMessages.map((messages, i) => {
+          const isNextMessageSamePerson =
+            combinedMessages[i - 1].isUserMessage ===
+            combinedMessages[i].isUserMessage
+
           if (i === combinedMessages.length - 1) {
             return <Message />
           } else {
@@ -45,9 +50,20 @@ const Messages = ({ fileId }: MessagesProps) => {
           }
         })
       ) : isLoading ? (
-        <div></div>
+        <div className="w-full flex flex-col gap-2">
+          <Skeleton className="h-16" />
+          <Skeleton className="h-16" />
+          <Skeleton className="h-16" />
+          <Skeleton className="h-16" />
+        </div>
       ) : (
-        <div></div>
+        <div className="flex-1 flex flex-col items-center justify-center gap-2">
+          <MessageSquare className="h-8 w-8 text-blue-500" />
+          <h3 className="font-semibold text-xl">You&apos;re all set!</h3>
+          <p className="text-zinc-500 text-sm">
+            Ask your first question to get started.
+          </p>
+        </div>
       )}
     </div>
   )
