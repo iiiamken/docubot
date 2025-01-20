d614987b7147f800b4d780f5e6cb338403f8110b
iiiamken <kenny.t.dinh93@gmail.com>
2025-01-20 23:46:04 +0100

    created loading message

--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -1,5 +1,6 @@
 import { trpc } from "@/app/_trpc/client"
 import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query"
+import { Loader2 } from "lucide-react"
 
 interface MessagesProps {
   fileId: string
@@ -15,7 +16,20 @@ const Messages = ({ fileId }: MessagesProps) => {
       getNextPageParam: (lastPage) => lastPage?.nextCursor,
     }
   )
-  return <div></div>
+
+  const loadingMessage = {
+    createdAt: new Date().toISOString(),
+    id: "loading-message",
+    isUserMessage: false,
+    text: (
+      <span className="flex h-full items-center justify-center">
+        <Loader2 className="h-4 w-4 animate-spin" />
+      </span>
+    ),
+  }
+  return (
+    <div className="flex max-h-[calc(100vh-3.5rem-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"></div>
+  )
 }
 
 export default Messages
