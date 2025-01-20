030999af9cd94fbdb1d713c93a9f47b0c90d1044
iiiamken <kenny.t.dinh93@gmail.com>
2025-01-20 19:19:05 +0100

    added message promt and formatted prevMessages

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -65,14 +65,44 @@ export const POST = async (req: NextRequest) => {
     take: 6,
   })
 
+  const formattedPrevMessages = prevMessages.map((msg) => ({
+    role: msg.isUserMessage ? ("user" as const) : ("assistant" as const),
+    content: msg.text,
+  }))
+
   //2. send to openAI to get response
   // const response = await openai.chat.completions.create({
   //   messages: query,
   // })
   const openaiResponse = await openai.chat.completions.create({
     model: "gpt-4o",
-    messages: [{ role: "user", content: "Say this is a test" }],
     stream: true,
     temperature: 0,
+    message: [
+      {
+        role: "system",
+        content:
+          "Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.",
+      },
+      {
+        role: "user",
+        content: `Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
+        
+  \n----------------\n
+  
+  PREVIOUS CONVERSATION:
+  ${formattedPrevMessages.map((message) => {
+    if (message.role === "user") return `User: ${message.content}\n`
+    return `Assistant: ${message.content}\n`
+  })}
+  
+  \n----------------\n
+  
+  CONTEXT:
+  ${results.map((r) => r.pageContent).join("\n\n")}
+  
+  USER INPUT: ${message}`,
+      },
+    ],
   })
 }
