d386bfa0bf87360052fd94b945b241baae55c3a9
iiiamken <kenny.t.dinh93@gmail.com>
2025-01-21 01:06:19 +0100

    created icon component and Icon rendering logic in message component

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -1,10 +1,28 @@
+import { cn } from "@/app/lib/utils"
+import { ExtendedMessages } from "@/types/message"
+
 interface MessageProps {
-  message: string
+  message: ExtendedMessages
   isNextMessageSamePerson: boolean
 }
 
 const Message = ({ message, isNextMessageSamePerson }: MessageProps) => {
-  return <div></div>
+  return (
+    <div
+      className={cn("flex items-end", { "justify-end": message.isUserMessage })}
+    >
+      <div
+        className={cn(
+          "relative flex h-6 w-6 aspect-square items-center justify-center",
+          {
+            "order-2 bg-blue-600 rounded-sm": message.isUserMessage,
+            "order-1 bg-zinc-800 rounded-sm": !message.isUserMessage,
+            invisible: isNextMessageSamePerson,
+          }
+        )}
+      ></div>
+    </div>
+  )
 }
 
 export default Message
