c601655381b454c30fefc6e83b28ba5817c5fa16
iiiamken <kenny.t.dinh93@gmail.com>
2025-01-21 16:17:03 +0100

    added onMutate to implement optimistic updates

--- src/components/chat/ChatContext.tsx
+++ src/components/chat/ChatContext.tsx
@@ -1,5 +1,7 @@
+import { trpc } from "@/app/_trpc/client"
+import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query"
 import { useMutation } from "@tanstack/react-query"
-import { ReactNode, createContext, useState } from "react"
+import { ReactNode, createContext, useRef, useState } from "react"
 
 type StreamResponse = {
   addMessage: () => void
@@ -24,6 +26,10 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
   const [message, setMessage] = useState<string>("")
   const [isLoading, setIsLoading] = useState<boolean>(false)
 
+  const utils = trpc.useContext()
+
+  const backupMessage = useRef("")
+
   const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMessage(e.target.value)
   }
@@ -43,6 +49,44 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
       }
 
       return response.body
+    },
+    onMutate: async () => {
+      backupMessage.current = message
+      setMessage("")
+
+      //1. cancel outgoing fetches
+      await utils.getFileMessages.cancel()
+
+      //2. get data in case we need to revert
+      const previousMessages = utils.getFileMessages.getInfiniteData()
+      //3. optimistically input data
+      utils.getFileMessages.setInfiniteData(
+        { fileId, INFINITE_QUERY_LIMIT },
+        (old) => {
+          //handling edge case of no old data exist
+          if (!old) {
+            return { pages: [], pageParams: [] }
+          }
+          //3.1 cloning old page
+          let newPages = [...old.pages]
+          let latestPage = newPages[0]!
+          //3.2 mutate pages messages
+          latestPage.messages = [
+            {
+              createdAt: new Date().toISOString(),
+              id: crypto.randomUUID(),
+              text: message,
+              isUserMessage: true,
+            },
+            ...latestPage.messages,
+          ]
+          //updating the last page to our new page with injected message
+          newPages[0] = latestPage
+
+          //overwrite the data in our api
+          return { ...old, pages: newPages }
+        }
+      )
     },
   })
 
