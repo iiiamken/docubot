dd1a88a44a3de027e1336a61e439fbd94dd8e0fc
iiiamken <kenny.t.dinh93@gmail.com>
2025-01-22 22:02:05 +0100

    added url and toast

--- src/components/Billingform.tsx
+++ src/components/Billingform.tsx
@@ -1,11 +1,27 @@
 "use client"
 
+import { trpc } from "@/app/_trpc/client"
 import { getUserSubscriptionPlan } from "@/app/lib/stripe"
+import { useToast } from "@/hooks/use-toast"
 
 interface BillingformProps {
   subscriptionPlan: Awaited<ReturnType<typeof getUserSubscriptionPlan>>
 }
 
-const Billingform = ({ subscriptionPlan }: BillingformProps) => {}
+const Billingform = ({ subscriptionPlan }: BillingformProps) => {
+  const { toast } = useToast()
+  const {} = trpc.createStripeSession.useMutation({
+    onSuccess: ({ url }) => {
+      if (url) window.location.href = url
+      if (!url) {
+        toast({
+          title: "There was a problem...",
+          description: "Please try again in a moment",
+          variant: "destructive",
+        })
+      }
+    },
+  })
+}
 
 export default Billingform
