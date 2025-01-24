96d3d21e433abbc5c5d9561b33a4ced8cf055c78
iiiamken <kenny.t.dinh93@gmail.com>
2025-01-24 15:39:42 +0100

    added user to db if subscribed first time

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -34,6 +34,18 @@ export async function POST(request: Request) {
     const subscription = await stripe.subscriptions.retrieve(
       session.subscription as string
     )
+
+    await db.user.update({
+      where: { id: session.metadata.userId },
+      data: {
+        stripeSubscriptionId: subscription.id,
+        stripeCustomerId: subscription.customer as string,
+        stripePriceId: subscription.items.data[0]?.price.id,
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000
+        ),
+      },
+    })
   }
 
   if (event.type === "invoice.payment_succeeded") {
