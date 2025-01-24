2aa55986d25ae0fc9e86057d30946bd9004ffd56
iiiamken <kenny.t.dinh93@gmail.com>
2025-01-24 16:17:00 +0100

    fixed type error

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -5,12 +5,12 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { notFound, redirect } from "next/navigation"
 
 interface PageProps {
-  params: { fileid: string }
+  params: Promise<{ fileid: string }> // `params` is asynchronous due to dynamic routing
 }
 
 const Page = async ({ params }: PageProps) => {
   //retrieve the file id
-  const { fileid } = params
+  const { fileid } = await params
   //make database call to get file data
   const { getUser } = getKindeServerSession()
   const user = await getUser()
