39a38087fd90189ab2af1142d62e50b19e84f213
iiiamken <kenny.t.dinh93@gmail.com>
2025-01-24 22:44:43 +0100

    removed description

--- src/components/Billingform.tsx
+++ src/components/Billingform.tsx
@@ -1,3 +1,89 @@
+// "use client"
+
+// import { trpc } from "@/app/_trpc/client"
+// import MaxWidthWrapper from "./MaxWidthWrapper"
+// import {
+//   Card,
+//   CardDescription,
+//   CardFooter,
+//   CardHeader,
+//   CardTitle,
+// } from "./ui/card"
+// import { Button } from "./ui/button"
+// import { Loader2 } from "lucide-react"
+// import { format } from "date-fns"
+// import { useToast } from "@/hooks/use-toast"
+// import { getUserSubscriptionPlan } from "@/app/lib/stripe"
+// import { useState } from "react"
+
+// interface BillingFormProps {
+//   subscriptionPlan: Awaited<ReturnType<typeof getUserSubscriptionPlan>>
+// }
+
+// const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
+//   const { toast } = useToast()
+//   const [isLoading, setIsLoading] = useState<boolean>(false)
+
+//   const { mutate: createStripeSession } = trpc.createStripeSession.useMutation({
+//     onSuccess: ({ url }) => {
+//       if (url) window.location.href = url
+//       if (!url) {
+//         toast({
+//           title: "There was a problem...",
+//           description: "Please try again in a moment",
+//           variant: "destructive",
+//         })
+//       }
+//       setIsLoading(false)
+//     },
+//     onMutate: () => setIsLoading(true),
+//   })
+
+//   return (
+//     <MaxWidthWrapper className="max-w-5xl">
+//       <form
+//         className="mt-12"
+//         onSubmit={(e) => {
+//           e.preventDefault()
+//           createStripeSession()
+//         }}
+//       >
+//         <Card>
+//           <CardHeader>
+//             <CardTitle>Subscription Plan</CardTitle>
+//             <CardDescription>
+//               You are currently on the <strong>{subscriptionPlan.name}</strong>{" "}
+//               plan.
+//             </CardDescription>
+//           </CardHeader>
+
+//           <CardFooter className="flex flex-col items-start space-y-2 md:flex-row md:justify-between md:space-x-0">
+//             <Button type="submit">
+//               {isLoading ? (
+//                 <Loader2 className="mr-4 h-4 w-4 animate-spin" />
+//               ) : null}
+//               {subscriptionPlan.isSubscribed
+//                 ? "Manage Subscription"
+//                 : "Upgrade to PRO"}
+//             </Button>
+
+//             {subscriptionPlan.isSubscribed ? (
+//               <p className="rounded-full text-xs font-medium">
+//                 {subscriptionPlan.isCanceled
+//                   ? "Your plan will be canceled on "
+//                   : "Your plan renews on"}
+//                 {format(subscriptionPlan.stripeCurrentPeriodEnd!, "dd.MM.yyyy")}
+//                 .
+//               </p>
+//             ) : null}
+//           </CardFooter>
+//         </Card>
+//       </form>
+//     </MaxWidthWrapper>
+//   )
+// }
+
+// export default BillingForm
 "use client"
 
 import { trpc } from "@/app/_trpc/client"
@@ -21,8 +107,9 @@ interface BillingFormProps {
 }
 
 const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
-  const { toast } = useToast()
   const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const { toast } = useToast()
 
   const { mutate: createStripeSession } = trpc.createStripeSession.useMutation({
     onSuccess: ({ url }) => {
@@ -38,7 +125,6 @@ const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
     },
     onMutate: () => setIsLoading(true),
   })
-
   return (
     <MaxWidthWrapper className="max-w-5xl">
       <form
@@ -52,8 +138,8 @@ const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
           <CardHeader>
             <CardTitle>Subscription Plan</CardTitle>
             <CardDescription>
-              You are currently on the <strong>{subscriptionPlan.name}</strong>{" "}
-              plan.
+              {/* You are currently on the <strong>{subscriptionPlan.name}</strong>{" "}
+              plan. */}
             </CardDescription>
           </CardHeader>
 
