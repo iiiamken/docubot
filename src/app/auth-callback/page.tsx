68424b8fc506f2dbc1bf8ade52248fce86cf3de6
iiiamken <kenny.t.dinh93@gmail.com>
2025-01-24 16:39:27 +0100

    added Suspense fallback for call-auth

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,30 +1,14 @@
 "use client"
-import { useRouter, useSearchParams } from "next/navigation"
-import { trpc } from "../_trpc/client"
 import { Loader2 } from "lucide-react"
-import { useEffect } from "react"
+import { useRouter, useSearchParams } from "next/navigation"
+import { Suspense, useEffect } from "react"
+import { trpc } from "../_trpc/client"
 
 const Page = () => {
   const router = useRouter()
   const searchParams = useSearchParams()
 
   const origin = searchParams.get("origin")
-
-  // DEPRECIATED USEQUERY METHOD
-  // trpc.authCallback.useQuery(undefined, {
-  //   onSuccess: (success) => {
-  //     if (success) {
-  //       router.push(origin ? `/${origin}` : "/dashboard")
-  //     }
-  //   },
-  //   onError: (err) => {
-  //     if (err.data?.code === "UNAUTHORIZED") {
-  //       router.push("/sign-in")
-  //     }
-  //   },
-  //   retry: true,
-  //   retryDelay: 500,
-  // })
 
   const { data, isError, error, isSuccess } = trpc.authCallback.useQuery(
     undefined,
@@ -49,13 +33,15 @@ const Page = () => {
   }, [isError, error, router])
 
   return (
-    <div className="w-full mt-24 flex justify-center">
-      <div className="flex flex-col items-center gap-2">
-        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
-        <h3 className="font-semibold text-xl">Setting up your account...</h3>
-        <p>You will be redirected automatically when completed.</p>
+    <Suspense fallback={<Loader2 className="h-5 w-5 animate-spin" />}>
+      <div className="w-full mt-24 flex justify-center">
+        <div className="flex flex-col items-center gap-2">
+          <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+          <h3 className="font-semibold text-xl">Setting up your account...</h3>
+          <p>You will be redirected automatically when completed.</p>
+        </div>
       </div>
-    </div>
+    </Suspense>
   )
 }
 
