c3bc4f09bfe0dab854ed638e9c296db94fc0d949
iiiamken <kenny.t.dinh93@gmail.com>
2025-01-25 01:03:32 +0100

    added check in billing page

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -3,6 +3,7 @@ import BillingForm from "@/components/Billingform"
 
 const Page = async () => {
   const subscriptionPlan = await getUserSubscriptionPlan()
+  if (!subscriptionPlan) return <div></div>
 
   return <BillingForm subscriptionPlan={subscriptionPlan} />
 }
