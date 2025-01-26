73fed934af4a506ddfac989bbda4e5b5611b7c76
iiiamken <kenny.t.dinh93@gmail.com>
2025-01-26 04:15:21 +0100

    test1

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
-import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware"
+import { authMiddleware } from "@kinde-oss/kinde-auth-nextjs/server"
 
 export const config = {
   matcher: ["/dashboard/:path*", "/auth-callback"],
 }
 
-export default withAuth
+export default authMiddleware
