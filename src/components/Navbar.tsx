e6385a531cf45b0370a969f399e00d0b6ad4af12
iiiamken <kenny.t.dinh93@gmail.com>
2025-02-01 17:11:18 +0100

    added id to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,14 +15,13 @@ const Navbar = async () => {
   const user = await getUser()
 
   return (
-    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+    <nav
+      id="navbar"
+      className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all"
+    >
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
-          <Link
-            id="home-icon-link"
-            href="/"
-            className="flex z-40 font-semibold"
-          >
+          <Link id="home-icon" href="/" className="flex z-40 font-semibold">
             <span>Dokubot.</span>
           </Link>
 
