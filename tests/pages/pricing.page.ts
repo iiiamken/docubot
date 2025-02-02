6c12dd3b4bf8ea2fe3598b372c83e8f31bef0a68
iiiamken <kenny.t.dinh93@gmail.com>
2025-02-02 23:07:56 +0100

    added locators and getters for upgrade link / signup link

--- tests/pages/pricing.page.ts
+++ tests/pages/pricing.page.ts
@@ -8,6 +8,8 @@ export class Pricing {
   private freePlan = this.page.locator("#Free-pricing-plan")
   private proPlan = this.page.locator("#Pro-pricing-plan")
   private pricingIntro = this.page.locator("#pricing-intro")
+  private signUpLink = this.page.locator("#sign-up-link")
+  private upgradeLink = this.page.locator("#upgrade-link")
 
   //getters
   getPricingTitle() {
@@ -24,6 +26,14 @@ export class Pricing {
 
   getPricingIntro() {
     return this.pricingIntro
+  }
+
+  getSignUpLink() {
+    return this.signUpLink
+  }
+
+  getUpgradeLink() {
+    return this.upgradeLink
   }
   //actions
   async navigateToPricingPage() {
