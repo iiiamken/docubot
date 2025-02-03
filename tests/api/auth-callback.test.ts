468e290bc9e659e8c8ea7119d5a0d95859273569
iiiamken <kenny.t.dinh93@gmail.com>
2025-02-04 00:01:05 +0100

    cleaned code

--- tests/api/auth-callback.test.ts
+++ tests/api/auth-callback.test.ts
@@ -1,16 +1,12 @@
 import test, { expect } from "@playwright/test"
 
 test.describe("api test cases for auth callback api endpoint", () => {
-  test.only("user not logged in returns UNAUTHORIZED", async ({ request }) => {
+  test("user not logged in returns UNAUTHORIZED", async ({ request }) => {
     const response = await request.get(
       "https://dokubot.vercel.app/api/trpc/authCallback"
     )
 
-    // Ensure the request returns an UNAUTHORIZED error
-
-    // Parse response JSON
     const data = await response.json()
-    // Verify error code is "UNAUTHORIZED"
     expect(data.error.data.code).toBe("UNAUTHORIZED")
   })
 
