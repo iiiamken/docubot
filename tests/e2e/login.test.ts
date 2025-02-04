c1592f10a63bc926fc0a57dd7894f3c490f5dadc
iiiamken <kenny.t.dinh93@gmail.com>
2025-02-04 23:26:02 +0100

    added id to dashboard title element

--- tests/e2e/login.test.ts
+++ tests/e2e/login.test.ts
@@ -1,15 +1,16 @@
-import { test, expect, chromium } from "@playwright/test"
+import { expect, test } from "@playwright/test"
 import { LoginPage } from "../pages/loginPage.page"
+import { kindePassword, kindeUsername } from "../test-data/test.data"
 
 test("Login and save session state", async ({ page }) => {
   const loginPage = new LoginPage(page)
 
   await loginPage.navigateToLoginPage()
-  await loginPage.login("your-username", "your-password")
+  await loginPage.login(kindeUsername, kindePassword)
 
   // Save session state
   await loginPage.saveSessionState()
 
   // Validate that login was successful
-  await expect(page.locator("#logged-in-element")).toBeVisible()
+  await expect(page.locator("#dashboard-title")).toBeVisible()
 })
