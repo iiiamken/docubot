4596ba238bc304138495522db3ea934f68a3385e
iiiamken <kenny.t.dinh93@gmail.com>
2025-02-05 22:26:17 +0100

    added test: pdf loads

--- tests/e2e/chat.test.ts
+++ tests/e2e/chat.test.ts
@@ -1,3 +1,39 @@
-import test from "@playwright/test"
+import test, { expect } from "@playwright/test"
+import { Chat } from "../pages/chat.page"
+import { LoginPage } from "../pages/loginPage.page"
+import {
+  chatPageUrl,
+  kindePassword,
+  kindeUsername,
+} from "../test-data/test.data"
 
-test.describe("test for chat page with test file item", () => {})
+test.describe("test for chat page with test file item", () => {
+  test("navigations to testfile works", async ({ page }) => {
+    const chatPage = new Chat(page)
+    await chatPage.navigateToChatPage()
+
+    const loginPage = new LoginPage(page)
+    await loginPage.login(kindeUsername, kindePassword)
+
+    const testFile = chatPage.getTestFile()
+
+    await testFile.click()
+
+    await expect(page).toHaveURL(chatPageUrl)
+  })
+
+  test("pdf loader works", async ({ page }) => {
+    const chatPage = new Chat(page)
+    await chatPage.navigateToChatPage()
+
+    const loginPage = new LoginPage(page)
+    await loginPage.login(kindeUsername, kindePassword)
+
+    const testFile = chatPage.getTestFile()
+    await testFile.click()
+
+    const pdfContent = chatPage.getPdfContent()
+
+    await expect(pdfContent).toBeVisible()
+  })
+})
--- tests/pages/chat.page.ts
+++ tests/pages/chat.page.ts
@@ -1,8 +1,21 @@
 import { Page } from "@playwright/test"
-import { chatPageUrl } from "../test-data/test.data"
+import { chatPageUrl, fileItemId } from "../test-data/test.data"
 
 export class Chat {
   constructor(private page: Page) {}
+
+  //locators
+  testFile = this.page.locator(fileItemId)
+  pdfContent = this.page.locator(".react-pdf__Page")
+
+  //getters
+  getTestFile() {
+    return this.testFile
+  }
+
+  getPdfContent() {
+    return this.pdfContent
+  }
 
   //actions
   async navigateToChatPage() {
