ea562decbed51231441448303bb9863fbe2ea4a9
iiiamken <kenny.t.dinh93@gmail.com>
2025-02-07 16:26:19 +0100

    fixed pricing tests

--- tests/pages/pricing.page.ts
+++ tests/pages/pricing.page.ts
@@ -1,21 +1,34 @@
-import { Page } from "@playwright/test"
+import { Locator, Page } from "@playwright/test"
 
 export class Pricing {
-  constructor(private page: Page) {}
+  private pricingTitle: Locator
+  private freePlan: Locator
+  private proPlan: Locator
+  private pricingIntro: Locator
+  private freeSignUpLink: Locator
+  private proSignUpLink: Locator
+  private freeUpgradeLink: Locator
+  private freeMaxPages: Locator
+  private proMaxPages: Locator
+  private navbar: Locator
+  private signIn: Locator
+  private registerLink: Locator
 
-  //locators
-  private pricingTitle = this.page.locator("#pricing-title")
-  private freePlan = this.page.locator("#Free-pricing-plan")
-  private proPlan = this.page.locator("#Pro-pricing-plan")
-  private pricingIntro = this.page.locator("#pricing-intro")
-  private freeSignUpLink = this.page.locator("#free-sign-up-link")
-  private proSignUpLink = this.page.locator("#pro-sign-up-link")
-  private freeUpgradeLink = this.page.locator("#free-upgrade-link")
-  private freeMaxPages = this.page.locator('[id="10 pages per PDF"]')
-  private proMaxPages = this.page.locator('[id="120 pages per PDF"]')
-  private navbar = this.page.locator("#navbar")
-  private signIn = this.page.locator("#sign-in-link")
-  private registerLink = this.page.locator("#register-link")
+  constructor(private page: Page) {
+    //locators
+    this.pricingTitle = this.page.locator("#pricing-title")
+    this.freePlan = this.page.locator("#Free-pricing-plan")
+    this.proPlan = this.page.locator("#Pro-pricing-plan")
+    this.pricingIntro = this.page.locator("#pricing-intro")
+    this.freeSignUpLink = this.page.locator("#free-sign-up-link")
+    this.proSignUpLink = this.page.locator("#pro-sign-up-link")
+    this.freeUpgradeLink = this.page.locator("#free-upgrade-link")
+    this.freeMaxPages = this.page.locator('[id="10 pages per PDF"]')
+    this.proMaxPages = this.page.locator('[id="120 pages per PDF"]')
+    this.navbar = this.page.locator("#navbar")
+    this.signIn = this.page.locator("#sign-in-link")
+    this.registerLink = this.page.locator("#register-link")
+  }
 
   //getters
   getPricingTitle() {
