982b0c98c62d379bd56462463b93c798b06c07f6
iiiamken <kenny.t.dinh93@gmail.com>
2025-02-10 04:46:44 +0100

    middleware test 1

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -4,24 +4,30 @@ import { initTRPC, TRPCError } from "@trpc/server"
 const t = initTRPC.create()
 const middleware = t.middleware
 
-const isAuth = middleware(
-  async (opts) => {
-    // 1
-    // const input = opts.getRawInput
-    //
-    // 2
-    // const input = opts.getRawInput as unknown as { id: string }
-    //
-    //3
-    // const input = opts.getRawInput as {
-    //   id?: string
-    //   email?: string
-    //   given_name?: string
-    //   family_name?: string
-    //   picture?: string
-    // }
+const isAuth = middleware(async (opts) => {
+  // 1
+  // const input = opts.getRawInput
+  //
+  // 2
+  // const input = opts.getRawInput as unknown as { id: string }
+  //
+  //3
+  // const input = opts.getRawInput as {
+  //   id?: string
+  //   email?: string
+  //   given_name?: string
+  //   family_name?: string
+  //   picture?: string
+  // }
+  const input = (await opts.input) as {
+    id?: string
+    email?: string
+    given_name?: string
+    family_name?: string
+    picture?: string
+  }
 
-    // if (!input) {
+  if (!input) {
     const { getUser } = getKindeServerSession()
     const user = await getUser()
     if (!user || !user.id) {
@@ -34,14 +40,13 @@ const isAuth = middleware(
       },
     })
   }
-  // return opts.next({
-  //   ctx: {
-  //     userId: input.id,
-  //     user: input,
-  //   },
-  // })
-  // })
-)
+  return opts.next({
+    ctx: {
+      userId: input.id,
+      user: input,
+    },
+  })
+})
 
 export const router = t.router
 export const publicProcedure = t.procedure
