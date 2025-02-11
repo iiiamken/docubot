c9f98ced7ec44702d48ddc1eb5eab3b545c6d96e
iiiamken <kenny.t.dinh93@gmail.com>
2025-02-11 04:02:40 +0100

    tests for getFileUploadStatus

--- tests/api/getFileUploadStatus.test.ts
+++ tests/api/getFileUploadStatus.test.ts
@@ -29,9 +29,7 @@ test.describe("api test cases for getFileUploadStatus api endpoint", () => {
     expect(data.result.data.status).toBe("SUCCESS")
   })
 
-  test.only("get file upload status with invalid file id returns the correct file", async ({
-    request,
-  }) => {
+  test("get file upload status with invalid file id", async ({ request }) => {
     const response = await request.post(
       "https://dokubot.vercel.app/api/trpc/getFileUploadStatus",
       {
@@ -39,7 +37,7 @@ test.describe("api test cases for getFileUploadStatus api endpoint", () => {
           "Content-Type": "application/json",
         },
         data: JSON.stringify({
-          fileId: "test",
+          fileId: "",
           user: {
             id: userId,
             email: kindeUsername,
@@ -53,6 +51,30 @@ test.describe("api test cases for getFileUploadStatus api endpoint", () => {
 
     const data = await response.json()
 
-    expect(data).toBe("test")
+    expect(data.error.data.code).toBe("UNAUTHORIZED")
+  })
+  test("get file upload status with invalid user", async ({ request }) => {
+    const response = await request.post(
+      "https://dokubot.vercel.app/api/trpc/getFileUploadStatus",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: JSON.stringify({
+          fileId: fileId,
+          user: {
+            id: "",
+            email: "",
+            given_name: "test",
+            family_name: "test",
+            picture: "test",
+          },
+        }),
+      }
+    )
+
+    const data = await response.json()
+
+    expect(data.error.data.code).toBe("UNAUTHORIZED")
   })
 })
